11c5b8cfbafd7907ef98e2e5d98adabc1b2343b6
Mahadi Hassan <itsmahadi@gmail.com>
2024-10-12 02:26:20 +0600

    update

--- apps/ecommerce/utils/generate_receipt.py
+++ apps/ecommerce/utils/generate_receipt.py
@@ -1,15 +1,16 @@
 from django.template.loader import get_template
 from django.core.files.base import ContentFile
-from weasyprint import HTML
+from xhtml2pdf import pisa
 from django.utils.timezone import make_aware
 import datetime
 
 from backend.utils.email_queue_manager import email_queue_overhauler
 from backend.utils.text_choices import EmailPriorityStatus
 
+
 def generate_receipt_for_cart(_confirmation_obj, context):
     try:
-        # Make pay_time timezone-aware
+        # Make pay_time timezone-aware if it's naive
         pay_time = _confirmation_obj.pay_time
         if pay_time is not None and isinstance(pay_time, datetime.datetime) and pay_time.tzinfo is None:
             pay_time = make_aware(pay_time)
@@ -18,16 +19,18 @@ def generate_receipt_for_cart(_confirmation_obj, context):
         html_template = get_template("Payment_Success_Receipt.html")
         html_content = html_template.render(context)
 
-        print("html content", html_content)
         print("creating pdf")
-        pdf = HTML(string=html_content).write_pdf()
-        print("pdf created")
-        if pdf:
-            pdf_name = f"Receipt_transID_{_confirmation_obj.pg_txnid}_{_confirmation_obj.mer_txnid}.pdf"
-            print("pdf name", pdf_name)
 
+        # Generate PDF using xhtml2pdf
+        pdf_name = f"Receipt_transID_{_confirmation_obj.pg_txnid}_{_confirmation_obj.mer_txnid}.pdf"
+        pdf_file = ContentFile(b"")  # Creating an empty file object to store PDF
+
+        # Use pisa to convert HTML to PDF
+        pisa_status = pisa.CreatePDF(html_content, dest=pdf_file)
+
+        if not pisa_status.err:
             cart_obj = _confirmation_obj.payment_request.cart
-            cart_obj.receipt.save(pdf_name, ContentFile(pdf), save=True)
+            cart_obj.receipt.save(pdf_name, pdf_file, save=True)
             print("pdf saved")
 
             email_queue_overhauler(
